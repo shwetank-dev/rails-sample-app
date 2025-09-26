debe84d3570c10a393ad42a3ab2eaa707203916d
shwetank-dev <shwetank.t.dev@gmail.com>
2025-09-26 17:47:13 -0400

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # GET /
-  root "application#hello"
+  # Static Pages
+  root "static_pages#home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
